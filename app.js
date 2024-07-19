a210b619a53e306811648eeda872c23558bd4dbb
thundacat <kahliel.jones.music@gmail.com>
2024-07-19 00:34:07 -0700

    I struggled with this deliverable really bad and only made it to the 5th exercise. I have to get more fluent on for loops and how to use them to update object information. I'm going to study this harder and resubmit this assignment if possilbe

---
+++ app.js
@@ -0,0 +1,33 @@
+// Excercise 5
+
+const pokemon = require('./data.js')
+
+const game = {
+  difficulty: ['Easy','Medium','Hard'],
+  party: [
+    pokemon[0],
+    pokemon[150],
+    pokemon[18],
+    pokemon[94],
+  ],
+  gyms: [
+    { location: "Pewter City", completed: false, difficulty: 1 },
+    { location: "Cerulean City", completed: false, difficulty: 2 },
+    { location: "Vermilion City", completed: false, difficulty: 3 },
+    { location: "Celadon City", completed: false, difficulty: 4 },
+    { location: "Fuchsia City", completed: false, difficulty: 5 },
+    { location: "Saffron City", completed: false, difficulty: 6 },
+    { location: "Cinnabar Island", completed: false, difficulty: 7 },
+    { location: "Viridian City", completed: false, difficulty: 8 },
+  ],
+  items: [
+    { name: "potion", quantity: 4 },
+    { name: "pokeball", quantity: 8 },
+    { name: "rare candy", quantity: 99 },
+  ],
+}
+
+// console.dir(pokemon, { maxArrayLength: null })
+
+// console.log(game)
+console.log(game.party)
